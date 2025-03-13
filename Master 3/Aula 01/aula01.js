ee173e231b011c50185dc36f60b966f1de538b91
MrGame021 <130846611+MrGame021@users.noreply.github.com>
2025-03-13 16:38:19 -0300

    Add files via upload

---
+++ Master 3/Aula 01/aula01.js
@@ -0,0 +1,24 @@
+// '===' -> comparador c/ tipagem
+// '==' -> comparador s/ tipagem
+// '<' -> menor que
+// '>' -> maior que
+// '<=' -> menor ou igual a
+// '>=' -> maior ou igual a
+// '!==' -> comparador de desigualdade c/ tipagem
+// '!=' -> comparador de desigualdade s/ tipagem
+
+
+a = 37299124675193
+b = 5721
+soma = a+b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+restoDivisao = a%b
+
+alert("A soma de "+a+" e "+b+" é igual a "+soma+"!\n\nA subtração de "+a+" e "+b+" é igual a "+subtracao+"!\n\nA multiplicação de "+a+" e "+b+" é igual a "+multiplicacao+"!\n\nA divisão de "+a+" e "+b+" é igual a "+divisao+"!\n\n resto da divisão de "+a+" e "+b+" é igual a "+restoDivisao+"!")
+document.write("A soma de "+a+" e "+b+" é igual a "+soma+"!")
+document.write("A subtração de "+a+" e "+b+" é igual a "+subtracao+"!")
+document.write("A multiplicação de "+a+" e "+b+" é igual a "+multiplicacao+"!")
+document.write("A divisão de "+a+" e "+b+" é igual a "+divisao+"!")
+document.write("O resto da divisão de "+a+" e "+b+" é igual a "+restoDivisao+"!")
