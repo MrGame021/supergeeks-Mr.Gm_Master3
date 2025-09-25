1976d3e8cf61b6d0c828fb2f77a099c438a71a26
MrGame021 <130846611+MrGame021@users.noreply.github.com>
2025-09-25 18:38:54 -0300

    Add files via upload

---
+++ Master 3/Aula 23 - exercícios_ptEXTRA/script.js
@@ -0,0 +1,179 @@
+function ex1(){
+    nome = document.getElementById('ex1-nome').value
+    document.getElementById('ex1-saida').innerHTML = 'Olá '+nome+'!'
+}
+function ex2(){
+    ex2_n1 = parseInt(document.getElementById('ex2-n1').value)
+    ex2_n2 = parseInt(document.getElementById('ex2-n2').value)
+    ex2_Soma = ex2_n1+ex2_n2
+
+    document.getElementById('ex2-saida').innerHTML = 'A soma dos números '+ex2_n1+' e '+ex2_n2+' é '+ex2_Soma+'.'
+}
+function ex3(){
+    ex3_n1 = parseInt(document.getElementById('ex3-n1').value)
+    if(ex3_n1 >= 0){
+        document.getElementById('ex3-saida').innerHTML = 'O número '+ex3_n1+' é <b>POSITIVO</b>'
+    }
+    else{
+        document.getElementById('ex3-saida').innerHTML = 'O número '+ex3_n1+' é <b>NEGAIVO</b>'
+    }
+}
+function ex4(){
+    ex4_t1 = document.getElementById('ex4-t1').value
+    t1Carac = ex4_t1.length
+
+    document.getElementById('ex4-saida').innerHTML = 'O texto escrito tem '+t1Carac+' caracteres'
+}
+function ex5(){
+    ex5_n1 = parseInt(document.getElementById('ex5-n1').value)
+    document.getElementById('ex5-saida').innerHTML = ''
+    for(i=1; i<=ex5_n1; i++){
+        document.getElementById('ex5-saida').innerHTML += i+' '
+    }
+}
+function ex6(){
+    cor = document.getElementById('cor').value
+    document.getElementById("ex6-saida").style.backgroundColor = cor
+}
+function ex7(){
+    ex7_n1 = Math.floor(Math.random()*20)+1
+    document.getElementById('ex7-saida').innerHTML = 'Número sorteado foi '+ex7_n1+'!'
+}
+ex8_lista = []
+function ex8(){
+    ex8_n1 = parseInt(document.getElementById('ex8-n1').value.trim())
+    ex8_lista.push(ex8_n1)
+    document.getElementById('ex8-saida').innerHTML = ex8_lista 
+}
+ex9_lista = []
+function ex9(){
+    ex9_n1 = parseInt(document.getElementById('ex9-n1').value.trim())
+    ex9_lista.push(ex9_n1)
+
+    ex9_quant = ex9_lista.length
+    ex9_maior = Math.max(...ex9_lista)
+    ex9_menor = Math.min(...ex9_lista)
+    ex9_soma = ex9_lista.reduce((a,b) => a+b)
+    ex9_media = ex9_soma/ex9_quant
+
+    document.getElementById('ex9-saida').innerHTML = 
+    '<b>'+ex9_lista+'</b><hr>Quantidade: '+ex9_quant+'<br>Maior: '+ex9_maior+'<br>Menor: '+ex9_menor+'<br>Soma: '+ex9_soma+'<br>Média: '+ex9_media.toFixed(2) 
+}
+function ex10(){
+    ex10_txt1 = document.getElementById('ex10-txt1').value
+
+    ex10_quant = (ex10_txt1.length)
+    ex10_frase = ex10_txt1.split(' ')
+    ex10_palav = ex10_frase.length
+    ex10_priCarac = ex10_txt1[0]
+    ex10_ultCarac = ex10_txt1[ex10_quant-1]
+    ex10_vogais = ex10_txt1.match(/[aeiouáéíóúàèìòùãõâêîôû]/gi).length
+
+    document.getElementById('ex10-saida').innerHTML =
+    ex10_txt1+
+    '<hr><b>Quantidade de catacteres:</b> '+ex10_quant+
+    '<br><b>Quantidade de palavras:</b> '+ex10_palav+
+    '<br><b>Primeiro caractere:</b> '+ex10_priCarac+
+    '<br><b>Último caractere:</b> '+ex10_ultCarac+
+    '<br><b>Quantidade de Vogais:</b>: '+ex10_vogais
+}
+function ex11(){
+    ex11_link = document.getElementById('ex11-link').value
+    document.getElementById('ex11-saida').style.backgroundImage = "url("+ex11_link+")"
+}
+ex12_contador = 0
+function ex12(){
+    ex12_contador++
+    document.getElementById('ex12-saida').innerHTML = ex12_contador
+}
+setInterval(ex12, 1000)
+function ex13(){
+    data = new Date
+    dias = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado']
+    meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+    document.getElementById('ex13-saida').innerHTML =
+    "Dia: "+data.getDate()+" ("+dias[data.getDay()]+
+    ")<br>Mês: "+meses[data.getMonth()]+
+    "<br>Ano: "+data.getFullYear()+
+    "<br><br>Horário: "+data.getHours()+":"+data.getMinutes()+":"+data.getSeconds()+":"+data.getMilliseconds()
+}
+pessoas = []
+function ex14(){
+    p = {
+        nome: document.getElementById('ex14-nome').value,
+        idade: parseInt(document.getElementById('ex14-idade').value),
+        cidade: document.getElementById('ex14-cidade').value,
+    }
+    pessoas.push(p)
+
+    document.getElementById('ex14-saida').innerHTML =
+    '<tr>'+
+        '<th>NOME</th>'+
+        '<th>IDADE</th>'+
+        '<th>CIDADE</th>'+
+    '<tr>'
+    for(i=0; i<pessoas.length; i++){
+        document.getElementById('ex14-saida').innerHTML +=
+            '<tr>'+
+                '<td>'+pessoas[i].nome+'</td>'+
+                '<td>'+pessoas[i].idade+'</td>'+
+                '<td>'+pessoas[i].cidade+'</td>'+
+            '<tr>'
+    }
+}
+function ex15(){
+    pcEscolha = Math.floor(Math.random()*3)
+    playerEscolha = document.getElementById('escolha').value
+    ex15PlayerEsc()
+    ex15Jogo()
+}
+function ex15PlayerEsc(){
+    if(playerEscolha == 0){
+        playerEsc = 'Pedra'
+    }
+    else if(playerEscolha == 1){
+        playerEsc = 'Papel'
+    }
+    else if(playerEscolha == 2){
+        playerEsc = 'Tesoura'
+    }
+}
+function ex15Jogo(){
+    if(playerEscolha == 0){                                                                                                                         // Pedra
+        if(pcEscolha == 0){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Pedra<br><br>EMPATE!'
+        }
+        else if(pcEscolha == 1){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Papel<br><br>VOCÈ PERDEU!'
+        }
+        else if(pcEscolha == 2){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Tesoura<br><br>VOCÊ GANHOU!'
+        }
+    }
+    else if(playerEscolha == 1){                                                                                                                    // Papel
+        if(pcEscolha == 0){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Pedra<br><br>VOCÊ VENCEU!'
+        }
+        else if(pcEscolha == 1){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Papel<br><br>EMPATE!'
+        }
+        else if(pcEscolha == 2){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Tesoura<br><br>VOCÊ PERDEU!'
+        }
+    }
+    else if(playerEscolha == 2){                                                                                                                    // Tesoura
+        if(pcEscolha == 0){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Pedra<br><br>VOCÊ PERDEU!'
+        }
+        else if(pcEscolha == 1){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Papel<br><br>VOCÈ GANHOU!'
+        }
+        else if(pcEscolha == 2){
+            document.getElementById('ex15-saida').innerHTML = 'Sua Escolha: '+playerEsc+'<br>Escolha do PC: Tesoura<br><br>EMPATE!'
+        }
+    }
+    else{
+        alert('Escolha alguma das opções existentes além da padrão')
+    }
+}
